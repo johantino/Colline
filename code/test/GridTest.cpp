583daf5e22a44c474687c2573e0c004b88315f42
s-martin <saimen54@hotmail.com>
2019-10-08 21:52:21 +0200

    added another unit test

--- code/test/GridTest.cpp
+++ code/test/GridTest.cpp
@@ -7,3 +7,17 @@ TEST_CASE("Grid_Ctr_001")
 	REQUIRE(grid1.getWidthX() == 5);
 	REQUIRE(grid1.getHightY() == 5);
 }
+
+TEST_CASE("getObservers_001")
+{
+	Grid grid1(3, 3);
+	REQUIRE(grid1.getWidthX() == 3);
+	REQUIRE(grid1.getHightY() == 3);
+
+	GridCell cell1;
+	Observable observable;
+	cell1.placeOccupier(&observable);
+
+	grid1.setCellAt(1, 1, &cell1);
+	auto res1 = grid1.getObservers(&cell1, 0, false);
+}
